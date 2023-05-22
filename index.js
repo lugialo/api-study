cc1194b0871c345b81ddef9ed944a81a2b96e053
lugialo <gabrielzin911@gmail.com>
2023-05-22 17:13:41 -0300

    API Atualizada

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = require('express');
+const express = require("express"); // importar o Node Express
+const app = express();
+const PORT = 8080; // Porta da API
+
+const teste = "lalala";
+
+app.listen(PORT, () => console.log(` ${teste} teste http:/localhost:${PORT}`));
+
+app.use(express.json());
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "👕",
+    size: "large",
+  });
+});
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({
+      message: "We need a logo!",
+    });
+  }
+
+  res.send({
+    tshirt: `👕 with your ${logo} and ID of ${id}`,
+  });
+});
