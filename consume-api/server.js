1bdf756b4cee5306fd71a78d349e03fd088cf85b
lugialo <gabrielzin911@gmail.com>
2023-05-29 17:54:59 -0300

    Atualizado para abrir pelo Nodemon

--- consume-api/server.js
+++ consume-api/server.js
@@ -1,10 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.get("/", (req, res) => {
-  return res.json([
-    { name: "Breno" },
-    { name: "Dove" }]);
+  return res.json([{ name: "Breno" }, { name: "Dove" }]);
 });
 
 app.listen("4567");
