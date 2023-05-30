45be5f48788e47bb77c3a81efc21aceb4ed6359e
lugialo <gabrielzin911@gmail.com>
2023-05-30 17:48:38 -0300

    Atualizações dia 30/5

--- consume-api/server.js
+++ consume-api/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(cors());
 app.get("/", (req, res) => {
-  return res.json([{ name: "Breno" }, { name: "Dove" }]);
+  return res.json([{ name: "Breno" }, { sub_name: "Dove" }]);
 });
 
 app.listen("4567");
