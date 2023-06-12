e76bfee8c5d73b215d9c72b5f3cd641411931278
lugialo <gabrielzin911@gmail.com>
2023-06-12 17:49:53 -0300

    Mudanças no back-end

--- consume-api/server.js
+++ consume-api/server.js
@@ -1,14 +1,19 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 app.use(cors());
-app.get("/", (req, res) => {
-  return res.json([
-    { nome: "Breno", adjetivo: "Lindo" },
-    { nome: "Gabriel", adjetivo: "Cabeçudo" },
-    { nome: "Zé", adjetivo: "Rayan" },
-  ]);
+app.get("/", async (req, res) => {
+  const { data } = await axios("https://jsonplaceholder.typicode.com/users");
+
+  return res.json(data);
+
+  // return res.json([
+  //   { nome: "Breno", adjetivo: "Lindo" },
+  //   { nome: "Gabriel", adjetivo: "Cabeçudo" },
+  //   { nome: "Zé", adjetivo: "Rayan" },
+  // ]);
 });
 
 app.listen("4567");
